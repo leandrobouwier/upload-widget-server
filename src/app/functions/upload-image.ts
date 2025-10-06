fde122245d1c2b5b6d51013dab1f3d5aa6fcb58a
Leandro Batista <leandrobouwierphp@gmail.com>
2025-10-06 16:57:56 -0300

    function error

--- src/app/functions/upload-image.ts
+++ src/app/functions/upload-image.ts
@@ -1,7 +1,9 @@
-import { db } from '@/infra/db'
-import { schema } from '@/infra/db/schemas'
 import { Readable } from 'node:stream'
 import { z } from 'zod'
+import { db } from '@/infra/db'
+import { schema } from '@/infra/db/schemas'
+import { type Either, makeLeft, makeRight } from '@/shared/either'
+import { InvalidFileFormat } from './errors/invalid-file-format'
 
 const uploadImageInput = z.object({
   fileName: z.string(),
@@ -12,11 +14,13 @@ const uploadImageInput = z.object({
 type UploadImageInput = z.input<typeof uploadImageInput>
 const allowedMimeTypes = ['image/jpg', 'image/jpeg', 'image/png', 'image/webp']
 
-export async function uploadImage(input: UploadImageInput) {
+export async function uploadImage(
+  input: UploadImageInput
+): Promise<Either<InvalidFileFormat, { url: string }>> {
   const { contentStream, contentType, fileName } = uploadImageInput.parse(input)
 
-  if(!allowedMimeTypes.includes(contentType)){
-    throw new Error('Invalid file format')
+  if (!allowedMimeTypes.includes(contentType)) {
+    return makeLeft(new InvalidFileFormat())
   }
 
   // TODO: carregar a imagem p/ o Cloudflare R2
@@ -26,4 +30,6 @@ export async function uploadImage(input: UploadImageInput) {
     remoteKey: fileName,
     remoteUrl: fileName,
   })
+
+  return makeRight({ url: '' })
 }
--- src/infra/http/routes/upload-image.ts
+++ src/infra/http/routes/upload-image.ts
@@ -1,7 +1,8 @@
+import { describe } from 'node:test'
 import type { FastifyPluginAsyncZod } from 'fastify-type-provider-zod'
-import { uploadImage } from '@/app/functions/upload-image'
 import { z } from 'zod'
-
+import { uploadImage } from '@/app/functions/upload-image'
+import { isRight, unwrapEither } from '@/shared/either'
 
 export const uploadImageRoute: FastifyPluginAsyncZod = async server => {
   server.post(
@@ -11,28 +12,38 @@ export const uploadImageRoute: FastifyPluginAsyncZod = async server => {
         summary: 'Upload an image',
         consumes: ['multipart/form-data'],
         response: {
-          201: z.object({ uploadId: z.string() }),
+          201: z.null().describe('image uploaded'),
           400: z.object({ message: z.string() }),
         },
       },
     },
     async (request, reply) => {
-      const  uploadedFile = await request.file({
-        limits: { fileSize: 1024 * 1024 * 2, // 2mb 
-        }
+      const uploadedFile = await request.file({
+        limits: {
+          fileSize: 1024 * 1024 * 2, // 2mb
+        },
       })
 
-      if(!uploadedFile){
-        return reply.status(400).send({ message: 'File is required'})
+      if (!uploadedFile) {
+        return reply.status(400).send({ message: 'File is required' })
       }
 
-     await uploadImage({
-      fileName: uploadedFile.filename,
-      contentType: uploadedFile.mimetype,
-      contentStream: uploadedFile.file,
-     })
+      const result = await uploadImage({
+        fileName: uploadedFile.filename,
+        contentType: uploadedFile.mimetype,
+        contentStream: uploadedFile.file,
+      })
 
-      return reply.status(201).send({ uploadId: 'teste' })
+      if (isRight(result)) {
+        return reply.status(201).send()
+      }
+
+      const error = unwrapEither(result)
+
+      switch (error.constructor.name) {
+        case 'InvalidFileFormat':
+          return reply.status(400).send({ message: error.message })
+      }
     }
   )
 }
