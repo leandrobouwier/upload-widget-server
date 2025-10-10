dfa036a71fe922281c56a37a4d35c73058d86799
Leandro Batista <leandrobouwierphp@gmail.com>
2025-10-10 11:50:00 -0300

    finalização upload-server

--- src/app/functions/export-uploads.spec.ts
+++ src/app/functions/export-uploads.spec.ts
@@ -1,12 +1,20 @@
 import { randomUUID } from 'crypto'
-import dayjs from 'dayjs'
-import { describe, expect, test } from 'vitest'
+import { describe, expect, test, vi } from 'vitest'
+import * as upload from '@/infra/storage/upload-file-to-storage'
 import { isRight, unwrapEither } from '@/shared/either'
 import { makeUpload } from '@/test/factories/make-upload'
 import { exportUploads } from './export-uploads'
-import { getUploads } from './get-uploads'
 
 describe('export uploads', () => {
+  const uploadStub = vi
+    .spyOn(upload, 'uploadFileToStorage')
+    .mockImplementationOnce(async () => {
+      return {
+        key: `${randomUUID()}.csv`,
+        url: 'http://example.com/file.csv',
+      }
+    })
+
   test('deve ser capaz de exportar uploads', async () => {
     const namePattern = randomUUID()
 
@@ -20,5 +28,39 @@ describe('export uploads', () => {
     const sut = await exportUploads({
       searchQuery: namePattern,
     })
+
+    const generatedCSVStream = uploadStub.mock.calls[0][0].contentStream
+    const csvAsString = await new Promise<string>((resolve, reject) => {
+      const chunks: Buffer[] = []
+
+      generatedCSVStream.on('data', (chunk: Buffer) => {
+        chunks.push(chunk)
+      })
+
+      generatedCSVStream.on('end', () => {
+        resolve(Buffer.concat(chunks).toString('utf-8'))
+      })
+
+      generatedCSVStream.on('error', err => {
+        reject(err)
+      })
+    })
+
+    const csvAsArray = csvAsString
+      .trim()
+      .split('\n')
+      .map(row => row.split(','))
+
+    expect(isRight(sut)).toBe(true)
+    expect(unwrapEither(sut).reportUrl).toBe('http://example.com/file.csv')
+    expect(csvAsArray).toEqual([
+      ['ID', 'Name', 'URL', 'Uploaded at'],
+      [upload1.id, upload1.name, upload1.remoteUrl, expect.any(String)],
+      [upload2.id, upload2.name, upload2.remoteUrl, expect.any(String)],
+      [upload3.id, upload3.name, upload3.remoteUrl, expect.any(String)],
+      [upload4.id, upload4.name, upload4.remoteUrl, expect.any(String)],
+      [upload5.id, upload5.name, upload5.remoteUrl, expect.any(String)],
+      [upload6.id, upload6.name, upload6.remoteUrl, expect.any(String)],
+    ])
   })
 })
--- src/app/functions/export-uploads.ts
+++ src/app/functions/export-uploads.ts
@@ -1,13 +1,12 @@
+import { PassThrough, Transform } from 'node:stream'
+import { pipeline } from 'node:stream/promises'
+import { stringify } from 'csv-stringify'
 import { ilike } from 'drizzle-orm'
 import { z } from 'zod'
-import { stringify } from 'csv-stringify'
 import { db, pg } from '@/infra/db'
 import { schema } from '@/infra/db/schemas'
-import { type Either, makeRight } from '@/shared/either'
-import { pipeline } from 'node:stream/promises'
 import { uploadFileToStorage } from '@/infra/storage/upload-file-to-storage'
-import { PassThrough } from 'node:stream'
-
+import { type Either, makeRight } from '@/shared/either'
 
 const exportUploadsInput = z.object({
   searchQuery: z.string().optional(),
@@ -41,20 +40,30 @@ export async function exportUploads(
   const cursor = pg.unsafe(sql, params as string[]).cursor(1)
 
   const csv = stringify({
-    delimiter: '',
+    delimiter: ',',
     header: true,
     columns: [
-      { key: 'id', header:'ID' },
-      { key: 'name', header:'Name' },
-      { key: 'remote_url', header:'URL' },
-      { key: 'created_at', header:'Uploaded at' },
+      { key: 'id', header: 'ID' },
+      { key: 'name', header: 'Name' },
+      { key: 'remote_url', header: 'URL' },
+      { key: 'created_at', header: 'Uploaded at' },
     ],
   })
 
   const uploadToStorageStream = new PassThrough()
 
-  const convertToCSVPipeline =  pipeline(
+  const convertToCSVPipeline = pipeline(
     cursor,
+    new Transform({
+      objectMode: true,
+      transform(chunks: unknown[], encoding, callback) {
+        for (const chunk of chunks) {
+          this.push(chunk)
+        }
+
+        callback()
+      },
+    }),
     csv,
     uploadToStorageStream
   )
@@ -66,12 +75,7 @@ export async function exportUploads(
     contentStream: uploadToStorageStream,
   })
 
-  const [{ url }] = await Promise.all([
-    uploadToStorage,
-    convertToCSVPipeline,
-  ])
-  
-  console.log(url)
+  const [{ url }] = await Promise.all([uploadToStorage, convertToCSVPipeline])
 
   return makeRight({ reportUrl: url })
 }
